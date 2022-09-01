32ae9fbd8c9af9a9b417eb6979780d8ee18188e2
felix1614 <105278671+felix1614@users.noreply.github.com>
2022-09-01 12:25:12 +0530

    Delete hacker directory

--- hacker/candles.py
+++
@@ -1,6 +0,0 @@
-candles_count = int(input().strip())
-
-a = list(map(int, input().rstrip().split()))
-b=max(a)
-print(b)
-print(a.count(b))
--- hacker/staircase.py
+++
@@ -1,7 +0,0 @@
-n=6
-p = n - 2
-for i in range(1, n ):
-    print(" " * p, "#" * i)
-    p -= 1
-print("#"*n)
-
