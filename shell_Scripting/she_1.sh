e5021b803d6ee7ee466d096bee55e419ea632e2a
felix1614 <105278671+felix1614@users.noreply.github.com>
2022-09-01 12:26:31 +0530

    Delete shell_Scripting directory

--- shell_Scripting/she_1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export(){
-  token="$(gettoken "afnanahmed7874@gmail.com" "Nanfa1614@#$" "wise")"
-  curl -L --silent --header "Authorization: GoogleLogin auth=$token" "http://spreadsheets.google.com/feeds/download/spreadsheets/Export?key=$1&exportFormat=$2&gid=$0"
-  echo ""
-}
-
-export 1ACz6hoBqhm0AI5IUbzsDw3IwGXCiwB2e-0gQF1BEKjo csv
--- shell_Scripting/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-shm_id=$(sha256sum  /home/afnan/Downloads/releases/alarm_service/alarms_events_service | awk '{print $1}')
-echo $shm_id
