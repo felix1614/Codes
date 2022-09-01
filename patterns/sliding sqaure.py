49b70060d9e4ed21bdf9ee7495dca75e60953bb9
felix1614 <105278671+felix1614@users.noreply.github.com>
2022-09-01 12:25:42 +0530

    Delete patterns directory

--- patterns/sliding sqaure.py
+++
@@ -1,4 +0,0 @@
-a=4
-for j in range(a):
-    if j!=0: print(" "*j,end="")
-    print("*"*a)
--- patterns/square_with_empty_centre.py
+++
@@ -1,8 +0,0 @@
-a=10
-for i in range(a):
-    for j in range(a):
-        if i==0 or i==a-1 or j==0 or j==a-1:
-            print("*",end="")
-        else:
-            print(" ",end="")
-    print()
